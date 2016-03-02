4c5f91619e35c7bf23b5b26743b0655686ea3b82
Nikola Majkic <n1kolaa924@gmail.com>
2016-03-02 01:13:20 +0100

    Revert "ion: retry during race with shrinking/allocating"
    This reverts commit 80ca9b5e0a3d0f6b48562dfd8f67af988cdf02c6.

--- drivers/gpu/ion/ion_cma_secure_heap.c
+++ drivers/gpu/ion/ion_cma_secure_heap.c
@@ -445,7 +445,6 @@ static struct ion_secure_cma_buffer_info *__ion_secure_cma_allocate(
 	ret = ion_secure_cma_alloc_from_pool(sheap, &info->phys, len);
 
 	if (ret) {
-retry:
 		ret = ion_secure_cma_add_to_pool(sheap, len);
 		if (ret) {
 			dev_err(sheap->dev, "Fail to allocate buffer\n");
@@ -454,9 +453,10 @@ retry:
 		ret = ion_secure_cma_alloc_from_pool(sheap, &info->phys, len);
 		if (ret) {
 			/*
-			 * Lost the race with the shrinker, try again
+			 * We just added memory to the pool, we shouldn't be
+			 * failing to get memory
 			 */
-			goto retry;
+			BUG();
 		}
 	}
 	mutex_unlock(&sheap->alloc_lock);
